6020b4f5e9312abd557eb7615ccebe00bc0242fd
haaaziq <haziqkhan123@gmail.com>
2020-08-20 00:05:58 +0530

    YelpCamp v9
    Prevented unauthenticated user from creating a new Campground, also associated User with the campground (campground schema now contain author, having username and id of signed in user).

--- models/campground.js
+++ models/campground.js
@@ -5,6 +5,14 @@ var campgroundSchema = new mongoose.Schema({
     name: String,
     image: String,
     description: String,
+    //Adding Association with User
+    author: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        username: String
+    },
     //Adding Association with Comment Schema
     comments: [
         {
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -17,23 +17,29 @@ router.get("/", function(req, res){
 });
 
 //NEW --> Form to create new Campground
-router.get("/new", function(req, res){
+router.get("/new", isSignedIn, function(req, res){
     res.render("campgrounds/new");
 });
 
 
 //CREATE --> Create new Campground in DB
-router.post("/", function(req, res){
+router.post("/", isSignedIn, function(req, res){
     //name, img and desc from form
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    }
     
     var newCampground = {
         name: name,
         image: image,
-        description: desc
+        description: desc,
+        author: author
     };
+    
     //Creating in DB
     Campground.create(newCampground, function(err, campground){
         if(err){
