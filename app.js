4501836700971c56603bde097ab90214900ebd35
haaaziq <haziqkhan123@gmail.com>
2020-09-12 01:14:27 +0530

    YelpCamp v12 Deployed
    Added start script, removed node_modules from git, and added MongoLab URL for database

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ const indexRoutes       = require("./routes/index"),
       commentRoutes     = require("./routes/comments"),
       campgroundRoutes  = require("./routes/campgrounds");
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true, useUnifiedTopology:true, useFindAndModify: false});
+// mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true, useUnifiedTopology:true, useFindAndModify: false});
+
+mongoose.connect("mongodb+srv://Haziq:haziqkhan123@hzqcluster.swczi.mongodb.net/yelpcamp?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology:true, useFindAndModify: false});
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
